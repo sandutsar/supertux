ac979b1b3d77059c465edd3ad8ecbb02d2300118
Ingo Ruhnke <grumbel@gmail.com>
2021-04-24 03:58:21 +0200

    Replace boost with standard C++

--- src/supertux/gameconfig.cpp
+++ src/supertux/gameconfig.cpp
@@ -16,6 +16,8 @@
 
 #include "supertux/gameconfig.hpp"
 
+#include <ctime>
+
 #include "config.h"
 
 #include "editor/overlay_widget.hpp"
@@ -320,6 +322,16 @@ Config::save()
   writer.end_list("addons");
 
   writer.end_list("supertux-config");
+}
+
+bool
+Config::is_christmas() const
+{
+  time_t rawtime;
+  std::time(&rawtime);
+  struct tm* timeinfo = std::localtime(&rawtime);
+  // 6. Dec, Saint Nicholas Day
+  return timeinfo->tm_mon == 11 && timeinfo->tm_mday >= 6;
 }
 
 /* EOF */
